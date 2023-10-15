a561541b5a3ee153f50378f884564cad1b9d6e9c
Daniyar <daniyarmu11@gmail.com>
2023-10-15 11:20:56 +0300

    initial

---
+++ Startup_School_Jojo/Hubs/CodeEditorHub.cs
@@ -0,0 +1,13 @@
+﻿using BlazorMonaco.Editor;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Startup_School_Jojo.Hubs
+{
+    public class CodeEditorHub : Hub
+    {
+        public async Task SendMessage(TextModel message, string text)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message, text);
+        }
+    }
+}
--- Startup_School_Jojo/Program.cs
+++ Startup_School_Jojo/Program.cs
@@ -4,6 +4,7 @@ using DateBaseLogic;
 using Startup_School_Jojo.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Startup_School_Jojo.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +13,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddRazorPages().AddRazorPagesOptions(options => options.RootDirectory = "/Layouts");
 builder.Services.AddSingleton<DB>();
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"))
-);
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+    $"Host={dbHost}; Database={dbName}; Username={dbUser}; Password={dbPassword}; Include Error Detail=True;"
+));
 
 var app = builder.Build();
 
@@ -26,13 +33,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<CodeEditorHub>("/codeEditor");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
